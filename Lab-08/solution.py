9a4120b6993431a5e8b34ecbb6a9b4baea5ea24f
AlextricOverload <alexwarren590@gmail.com>
2026-04-02 15:48:23 -0600

    adjacency complete, import heapq

--- Lab-08/solution.py
+++ Lab-08/solution.py
@@ -5,12 +5,22 @@
 # Last Modified (YYYY-MM-DD): 2026-04-02
 
 # Imports
+import heapq
 from typing import Dict, List, Tuple
 
 def signal_times(times: List[List[int]], n: int, k: int) -> int:
-
+    graph = adjacency(times)
     return 0
 
-def adjacency(times: List[List[int]]) -> Dict[int, List[Tuple[int, int]]]:
 
+def adjacency(times: List[List[int]]) -> Dict[int, List[Tuple[int, int]]]:
     adj = {}
+
+    for edge in times:
+        src, dest, time = edge
+        if src not in adj:
+            adj[src] = []
+
+        adj[src].append((dest, time))
+
+    return adj
