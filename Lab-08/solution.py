9f4a590e87f05bc964713dd94022c00a6a6eab7c
AlextricOverload <alexwarren590@gmail.com>
2026-04-02 15:45:33 -0600

    Completed imports, began adjacency function

--- Lab-08/solution.py
+++ Lab-08/solution.py
@@ -5,8 +5,12 @@
 # Last Modified (YYYY-MM-DD): 2026-04-02
 
 # Imports
-from typing import List
+from typing import Dict, List, Tuple
 
 def signal_times(times: List[List[int]], n: int, k: int) -> int:
 
     return 0
+
+def adjacency(times: List[List[int]]) -> Dict[int, List[Tuple[int, int]]]:
+
+    adj = {}
