21a6db7b70cc6675883aa68d9ffc0c4d8dbae8cd
AlextricOverload <alexwarren590@gmail.com>
2026-04-02 15:38:50 -0600

    began signal_times function

--- Lab-08/solution.py
+++ Lab-08/solution.py
@@ -4,3 +4,9 @@
 # Author: Alexander Warren
 # Last Modified (YYYY-MM-DD): 2026-04-02
 
+# Imports
+from typing import List
+
+def signal_times(times: List[List[int]], n: int, k: int) -> int:
+
+    return 0
