6456c9c11652910e0f8e6c7e371624604e9a3200
AlextricOverload <alexwarren590@gmail.com>
2026-04-28 10:36:07 -0600

    Docstring explaining probability

--- Lab-12/solution.py
+++ Lab-12/solution.py
@@ -3,3 +3,10 @@
 # Lab Practice 12
 # Author: Alexander Warren
 # Last Modified(YYYY-MM-DD): 2026-04-28
+
+"""
+P(a,b,n) = probability that Player A wins from state (a,b)
+= 1.0 if a == n (A already won)
+= 0.0 if b == n (B already won)
+= 0.5 * P(a+1,b,n) + 0.5 * P(a,b+1,n) otherwise
+"""
